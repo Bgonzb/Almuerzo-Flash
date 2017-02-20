60850dd3609a2364533ad2e2a5465fac131a25c1
Eduardo <e.osoriovenegas@gmail.com>
2017-02-20 14:42:37 -0300

    creacion del controller login/logueo, registrar/usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'registrar/usuarios'
+
+  get 'login/logueo'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
