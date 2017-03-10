7f1a3aedad65bb985211bf8c2f27ad040a1d86eb
Eduardo <e.osoriovenegas@gmail.com>
2017-03-10 14:36:06 -0300

    cambiando el nombre a los links

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :nombre, presence:true
+  validates :phone, presence:true, numericality:{greater_than: 0, only_integer:true}
+  validates :tienda, presence:true
+
 end
