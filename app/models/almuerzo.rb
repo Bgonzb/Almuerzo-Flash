ec4e1c9160511d65cc09381a2d108dbb7e351695
Eduardo <e.osoriovenegas@gmail.com>
2017-03-10 12:57:56 -0300

    creacion de las migraciones en la tabla user phone,nombre,tienda

--- app/models/almuerzo.rb
+++ app/models/almuerzo.rb
@@ -1,7 +1,7 @@
 class Almuerzo < ActiveRecord::Base
 	validates :foto, presence:true
-	validates :nombre, presence:true
-	validates :descripcion, presence:true
-	validates :precio, presence:true
-	validates :stock, presence:true
+	validates :nombre, presence:true, uniqueness:true
+	validates :descripcion, presence:true, length:{ minunum: 10, maximum:100}
+	validates :precio, presence:true, numericality:{greater_than: 0, only_integer:true} 
+	validates :stock, presence:true, numericality:{greater_than: 0, only_integer:true}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221085821) do
+ActiveRecord::Schema.define(version: 20170310145506) do
 
   create_table "almuerzos", force: :cascade do |t|
     t.string   "foto"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20170221085821) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone"
+    t.string   "nombre"
+    t.string   "tienda"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
